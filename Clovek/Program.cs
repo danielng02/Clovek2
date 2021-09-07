54a8161a56cd7814fd9d0fd340753fd9075b51df
Nguyen Manh Hung <nguyen.2018@ssps.cz>
2021-09-07 13:51:26 +0200

    Přidejte soubory projektu.

---
+++ Clovek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clovek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
